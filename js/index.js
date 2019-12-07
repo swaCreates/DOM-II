101c27d9ed603b76b86daff49e0b5e0ebf9a9443
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2019-12-06 19:25:52 -0500

    added event on navanchors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+// Nav Anchors #1 //
+
+const anchorHover= document.querySelectorAll('.nav-link');
+
+anchorHover.forEach(function(anchor){
+    anchor.addEventListener('mouseover', function(event){
+        event.target.style.color= 'red';
+        event.target.style.textTransform= 'uppercase';
+    })
+    anchor.addEventListener('mouseout', function(event){
+        event.target.style.color= '';
+        event.target.style.textTransform= '';
+    })
+})
+

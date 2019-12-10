6799ed066afb0fafc4093ad5ecdb1a90e2197c4d
Francois Chery <swa_isthecreator@Francoiss-MBP.home>
2019-12-10 16:07:28 -0500

    almost completed. Need one more event

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-// Nav Anchors #1 //
+// Nav Anchor (Hover) #1 //
 
 const anchorHover= document.querySelectorAll('.nav-link');
 
@@ -8,20 +8,22 @@ anchorHover.forEach(function(anchor){
     anchor.addEventListener('mouseover', function(event){
         event.target.style.color= 'red';
         event.target.style.textTransform= 'uppercase';
+        event.target.style.transform= 'rotate(180deg)';
     })
     anchor.addEventListener('mouseout', function(event){
         event.target.style.color= '';
         event.target.style.textTransform= '';
+        event.target.style.transform= 'rotate(0deg)'
     })
 })
 
-// Document #2 //
+// Document (Key Down) #2 //
 
 document.addEventListener('keydown', function(event){
     console.log(document.textContent+= event.code);
 })
 
-// Buttons #3 //
+// Buttons (Click) #3 //
 
 const buttons= document.querySelectorAll('.btn');
 
@@ -29,6 +31,7 @@ buttons.forEach(function(btn){
     btn.addEventListener('click', function(event){
         event.target.style.backgroundColor= 'red';
         event.target.style.color='white';
+        
     })
     btn.addEventListener('mouseout', function(event){
         event.target.style.backgroundColor= '';
@@ -36,7 +39,7 @@ buttons.forEach(function(btn){
     })
 })
 
-// Images #4 //
+// Images (Dbl Click) #4 //
 
 const image= document.querySelectorAll('img');
 
@@ -47,6 +50,59 @@ image.forEach(function(img){
     })
     img.addEventListener('mouseout', function(event){
         event.target.style.transform ='';
-        event.style.transition ='';
+        event.style.transition ='0';
+    })
+})
+
+// (Page Scroll) #5 //
+
+window.addEventListener('scroll', function(){
+    this.console.log('User is scrolling');
+})
+
+// Window Resize #6 //
+
+window.addEventListener('resize', function(){
+    this.console.log('Random screen-sizing: ' + Math.random());
+})
+
+// Error Alert #7 //
+
+const links= document.querySelectorAll('.nav-link');
+
+links.forEach(function(err){
+    err.addEventListener('click', function(){
+        alert('Error! This page has been hacked. Nothing works!!!.');
+    })
+})
+
+// Fun Bus title #8 //
+
+const title= document.querySelector('.logo-heading')
+
+title.addEventListener('mouseover', function(event){
+    event.target.style.transform= 'rotate(180deg)';
+})
+
+title.addEventListener('mouseout', function(event){
+    event.target.style.transform= 'rotate(0deg)';
+})
+// Page Load #9 //
+
+window.addEventListener('load', function(){
+    alert('Loading...Loading...Loading.....Loaded :) ');
+});
+
+// Copy P tag #10 //
+
+
+
+// Prevent Default //
+
+const anchors= document.querySelectorAll('.nav-link');
+
+anchors.forEach(function(links){
+    links.addEventListener('click', function(event){
+        event.preventDefault();
     })
 })
